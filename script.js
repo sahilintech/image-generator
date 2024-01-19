156a63ec04674579e5d4782dc4a0314b90c76ed2
Sahil Chauhan <sahilchauhan45448@gmail.com>
2024-01-19 21:46:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const generateForm = document.querySelector(".generate-form");
 const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
 
-const OPENAI_API_KEY = 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
+const OPENAI_API_KEY = "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";
 let isImageGenerating = false;
 
 const updateImageCard = (imgDataArray) => {
